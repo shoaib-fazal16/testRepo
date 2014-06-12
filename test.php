108f677b18eafac872b6ea3ca827f2b67c46ba5a
Shoaib Fazal <shoaib.fazal@rolustech.com>
2014-06-12 13:19:20 +0500

    Dirty Code!

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "I am a test file I was created by a developer and deployed to webServer for test purposes.<br />";
+echo "I am a test file I was created by a developer and deployed to webServer for test purposes.<br />" //Oops I am a dirty Code
 
 
 // I am build number 15
